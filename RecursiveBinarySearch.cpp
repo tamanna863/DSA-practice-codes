90d81cb18134dbc2ccb55328bd40ca4d93bb3299
tamanna863 <75742463+tamanna863@users.noreply.github.com>
2022-07-04 11:52:29 +0530

    Update RecursiveBinarySearch.cpp

--- RecursiveBinarySearch.cpp
+++ RecursiveBinarySearch.cpp
@@ -1,4 +1,4 @@
-//recursive waala
+
 
 #include <bits/stdc++.h>
 
@@ -30,40 +30,5 @@ int main()
 
     return 0;
 }
-/
 
-#include <iostream>
-
-using namespace std;
-int binS(int *arr,int e, int key){
-    int s = 0;
-     int mid =  s + (e-s)/2;
-    while(s<=e){
-        if(arr[mid]==key){
-            return mid;
-        } 
-        if(key > arr[mid]){
-            s = mid +1;
-        }
-        else{
-            e = mid -1;
-        }
-        mid = s+(e-s)/2;
-    }
-    return -1;
-}
-
-int main()
-{
-    int arr[5]= {2,3,5,7,8};
-    int key;
-    cout<<"enter the key to be searched";
-    cin>>key;
-    
-    
-   cout<< binS(arr,4,key);
-    
-
-    return 0;
-}   */
 
